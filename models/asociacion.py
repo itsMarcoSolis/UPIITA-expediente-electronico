7c9599d5147482cb19e08833ef9bd6f146eaa7db
Marco Solis <this.solis@ieee.org>
2025-02-16 16:05:38 -0600

    add asociaciones model methods

--- models/asociacion.py
+++ models/asociacion.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from database import Base
+from database import Base, obtener_sesion
 
 class Asociacion(Base):
     __tablename__ = "asociaciones"
@@ -9,3 +9,18 @@ class Asociacion(Base):
     asesor = Column(String, nullable=False)
     miembros = relationship("MiembroAsociacion", back_populates="asociacion")
     grupos = relationship("Grupo", back_populates="asociacion")
+
+    @staticmethod
+    def agregar_asociacion(nombre: str, asesor: str):
+        session = obtener_sesion()
+        nueva_asociacion = Asociacion(nombre=nombre, asesor=asesor)
+        session.add(nueva_asociacion)
+        session.commit()
+        session.close()
+
+    @staticmethod
+    def obtener_asociaciones():
+        session = obtener_sesion()
+        asociaciones = session.query(Asociacion).all()
+        session.close()
+        return asociaciones
